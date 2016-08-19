64e938c2c22d8a1d21e2ebbfa313753346547298
Artem-Vorobiov <artem-vorobiov_pavlovich@mail.ru>
2016-08-19 20:12:19 +0300

    Fix: email, phone validate; coz that block sendgrid send me letter

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,11 +1,11 @@
 class Contact < MailForm::Base
 	attribute :name,      :validate => true
-  attribute :email,     :validate => /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+  attribute :email,     :validate => true
   attribute :file,      :attachment => true
 
   attribute :message
   attribute :nickname,  :captcha  => true
-  attribute :phone,     :validate => /(?<!\w)(?:(?:(?:(?:\+?3)?8\W{0,5})?0\W{0,5})?[34569]\s?\d[^\w,;(\+]{0,5})?\d\W{0,5}\d\W{0,5}\d\W{0,5}\d\W{0,5}\d\W{0,5}\d\W{0,5}\d(?!(\W?\d))/x
+  attribute :phone,     :validate => true
 
   def headers
     {
